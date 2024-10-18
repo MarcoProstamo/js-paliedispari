4c61c97a11d316734a0bd55a4c0f1283aabe06a5
MarcoProstamo <Marcopros1201@gmail.com>
2024-10-18 14:57:10 +0200

    Palindroma → Function and Minor Fixes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
-const userWord = // prompt("Enter the word to check");
-  isPalindrome
-    ? console.log(`${userWord} is Palindrome`)
-    : console.log(`${userWord} isn't Palindrome`);
+function isPalindrome(userWord) {
+  userWord = userWord.toLowerCase();
+
+  const userWordArray = [];
+  const userReversedWordArray = [];
+  for (let i = 0; i < userWord.length; i++) {
+    const char = userWord.charAt(i);
+    userWordArray.push(char);
+    userReversedWordArray.unshift(char);
+  }
+
+  let isUserWordPalindrome = true;
+  for (let i = 0; i < userWordArray.length; i++) {
+    userWordArray[i] !== userReversedWordArray[i]
+      ? (isUserWordPalindrome = false)
+      : null;
+  }
+
+  return isUserWordPalindrome;
+}
+
+const userWord = prompt("Enter the word to check");
+isPalindrome(userWord)
+  ? console.log(`\'${userWord}\' is Palindrome`)
+  : console.log(`\'${userWord}\' isn't Palindrome`);
